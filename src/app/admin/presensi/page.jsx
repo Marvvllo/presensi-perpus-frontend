c4fa52b1026518bdd02d77e921be8a53ee875ae2
Marvvllo <marvtfs@gmail.com>
2023-12-05 13:23:55 +0800

    Added sharp for image optimization

--- src/app/admin/presensi/page.jsx
+++ src/app/admin/presensi/page.jsx
@@ -11,21 +11,7 @@ const Presensi = () => {
           href="/admin/presensi/create"
           className="inline-flex flex-row gap-2 rounded-md px-4 py-1 my-2 bg-primary text-white font-medium hover:bg-transparent hover:text-primary transition outline-1 outline outline-primary"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-4 h-auto"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 4.5v15m7.5-7.5h-15"
-            />
-          </svg>
-          Tambah
+          + Tambah
         </Link>
       </section>
 
